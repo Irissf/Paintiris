0e76d239decc8ad65bae329b17650425ee32dfe0
Irissf <iris.seijo@gmail.com>
2021-08-13 10:09:50 +0200

    Dia 2 estilo del TabControl, botones y tabItem

--- Paintiris/MainWindow.xaml.cs
+++ Paintiris/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,15 +29,46 @@ namespace Paintiris
         public MainWindow()
         {
             InitializeComponent();
+           
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            
             Canvas lienzo = new Canvas();
             lienzo.Background = Brushes.White;
             lienzo.Height = altoLienzo;
             lienzo.Width = anchoLienzo;
-            folio.Children.Add(lienzo);
+            gr_folio.Children.Add(lienzo);
+        }
+
+        /// <summary>
+        /// Cada vez que se cambia una de las pestañas del menú
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuPestanhas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //un switch con los diferentes menus, crearemos una clase por cada uno de ellos
+            /*averiguamos que pestañita tenemos marcada*/
+            
+            if (ti_inicio.IsSelected)
+            {
+                //Trace es una alternativa a console.writeline
+                Trace.WriteLine("inicio");
+            }
+            if (ti_archivo.IsSelected)
+            {
+                Trace.WriteLine("archivo");
+            }
+            if (ti_estudio.IsSelected)
+            {
+                Trace.WriteLine("estudio");
+            }
+            if (ti_ver.IsSelected)
+            {
+                Trace.WriteLine("ver");
+            }
         }
     }
 }
