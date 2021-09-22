ccba7a33eb3e06469d185e33056d12e518fd66f8
Irissf <iris.seijo@gmail.com>
2021-09-22 13:25:48 +0200

    Lapiz o pincel y tamaño de los pinceles

--- Paintiris/MainWindow.xaml.cs
+++ Paintiris/MainWindow.xaml.cs
@@ -28,15 +28,18 @@ namespace Paintiris
         //**************************************** PROPIEDADES ****************************************************
 
         //Control del dibujo por parte de la clase pincel
-        public InkCanvas lienzo;
-        public bool dibujar;
-        public bool borrar;
+        public InkCanvas lienzo; //el area de dibujo
+        public bool siPincel; // si pincel está activado, dividimos el alto entre dos para el ancho 
+        public bool pintarActivado; //marcamos si está activado un pincel para el cambio de color
+        int altoPincel = 1; //alto del pincel
+        int anchoPincel = 1; //ancho del pincel
+
+        //Gestionar el color
         SolidColorBrush colorPintar = new SolidColorBrush(Color.FromArgb(255,0,0,0));
         Color colorPin;
 
         //Clase gestión pinceles
         Pinceles pinceles;
-
 
         //Clase gestión de los archivos
         Archivo archi;
@@ -59,10 +62,8 @@ namespace Paintiris
             archi = new Archivo(lienzo);
             pinceles = new Pinceles(this);
 
-            //llenamos colecciones
-            prueba.Add(tbtn_borrar);
-            prueba.Add(tbtn_pintar);
-            prueba.Add(tbtn_selec);
+            //llenamos colecciones para que al activar uno se desactiven todos
+            llenarColecciones();
 
             //ponemos color al inicio del pincel
             colorPin = Color.FromArgb(255, 0, 0, 0);
@@ -75,7 +76,7 @@ namespace Paintiris
         //BOTONES__________________________________________________________________
 
         /// <summary>
-        /// Click de los elementos de archivo
+        /// Click de los elementos de archivo => NUEVO, GUARDAR, GUARDARCOMO, ABRIR
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -118,7 +119,11 @@ namespace Paintiris
         }
 
 
-
+        /// <summary>
+        /// Gestionamos el cambio de color, al pulsar nos abre un formulario nuevo para elegir el color
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             //le mandamos al dialog el color que tenemos en el canvas al inicio
@@ -129,15 +134,16 @@ namespace Paintiris
                 colorPintar = new SolidColorBrush(miColor.color);
                 cv_colorPintar.Background = colorPintar;
                 //si el pincel está seleccionado, volvemos a crearlo o no se actualizará el nuevo color
-                if (tbtn_pintar.IsChecked == true)
+                if (pintarActivado == true)
                 {
                     lienzo.EditingMode = InkCanvasEditingMode.Ink;
-                    lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(5, 5, colorPintar.Color);
+                    lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
                 }
             }
         }
 
-        //prueba a botonoes como radio button
+
+        //prueba a botones como radio button
         private void tbs_Checked(object sender, RoutedEventArgs e)
         {
             ToggleButton tbtn = (ToggleButton)sender;
@@ -152,23 +158,106 @@ namespace Paintiris
 
             switch (tbtn.Name)
             {
-                case "tbtn_pintar":
-                    lienzo.EditingMode = InkCanvasEditingMode.Ink;
-                    lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(5, 5, colorPintar.Color);
-                    break;
                 case "tbtn_borrar":
                     Trace.WriteLine("borrar");
                     lienzo.EditingMode = InkCanvasEditingMode.EraseByPoint;
                     //propiedades de la goma
                     lienzo.EraserShape = new EllipseStylusShape(1, 1);
+                    pintarActivado = false;
                     break;
                 case "tbtn_selec":
                     lienzo.EditingMode = InkCanvasEditingMode.Select;
+                    pintarActivado = false;
                     break;
 
                 default:
                     break;
             }
+        }
+
+        private void pintar_checked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton tbtn = (ToggleButton)sender;
+           
+
+            foreach (ToggleButton boton in pincelTamano)
+            {
+                if (boton.Name != tbtn.Name)
+                {
+                    boton.IsChecked = false;
+                }
+            }
+            switch (tbtn.Name)
+            {
+                case "tbtn_pincel1px":
+                    altoPincel = 1;
+                    break;
+                case "tbtn_pincel3px":
+                    altoPincel = 3;
+                    break;
+                case "tbtn_pincel6px":
+                    altoPincel = 6;
+                    break;
+                case "tbtn_pincel10px":
+                    altoPincel = 10;
+                    break;
+                case "tbtn_pincel13px":
+                    altoPincel = 13;
+                    break;
+               
+
+                default:
+                    break;
+            }
+
+            pintarActivado = true;
+            lienzo.EditingMode = InkCanvasEditingMode.Ink;
+            lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
+        }
+
+        private void pincelOlapiz_Checked(object sender, RoutedEventArgs e)
+        {
+            ToggleButton tbtn = (ToggleButton)sender;
+            // si le da a uno los otros los desmarcamos
+            foreach (ToggleButton boton in herramienta)
+            {
+                if (boton.Name != tbtn.Name)
+                {
+                    boton.IsChecked = false;
+                }
+            }
+
+            //si está marcado que es un pincel, dividimos el alto entre dos para hacerlo de forma ovalada
+            if (tbtn.Name == "tbtn_pincel")
+            {
+                siPincel = true;
+                anchoPincel = altoPincel / 2;
+            }
+            else
+            {
+                siPincel = false;
+                anchoPincel = altoPincel;
+            }
+            lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
+
+        }
+
+        //********************************************* FUNCIONES ************************************************
+
+        //LLENAR Colecciones__________________________________________________________________
+        private void llenarColecciones()
+        {
+            prueba.Add(tbtn_borrar);
+            prueba.Add(tbtn_selec);
+
+            pincelTamano.Add(tbtn_pincel1px);
+            pincelTamano.Add(tbtn_pincel3px);
+            pincelTamano.Add(tbtn_pincel6px);
+            pincelTamano.Add(tbtn_pincel10px);
+            pincelTamano.Add(tbtn_pincel13px);
+
+            herramienta.Add(tbtn_lapiz);
+            herramienta.Add(tbtn_pincel);
         }
     }
 
