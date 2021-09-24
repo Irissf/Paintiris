5463ef538999aa2b94d1c82ac8089759ffe4ee91
Irissf <iris.seijo@gmail.com>
2021-09-24 20:10:11 +0200

    Paleta de colores inicial

--- Paintiris/MainWindow.xaml.cs
+++ Paintiris/MainWindow.xaml.cs
@@ -25,14 +25,19 @@ namespace Paintiris
     public partial class MainWindow : Window
     {
 
-        //**************************************** PROPIEDADES ****************************************************
+        //********************************************************************************************
+                    /*  ___  ____ ____ ___  _ ____ ___  ____ ___  ____ ____ 
+                        |__] |__/ |  | |__] | |___ |  \ |__| |  \ |___ [__  
+                        |    |  \ |__| |    | |___ |__/ |  | |__/ |___ ___] */
+
+        //********************************************************************************************
 
         //Control del dibujo por parte de la clase pincel
         public InkCanvas lienzo; //el area de dibujo
         public bool siPincel; // si pincel está activado, dividimos el alto entre dos para el ancho 
         public bool pintarActivado; //marcamos si está activado un pincel para el cambio de color
-        int altoPincel = 1; //alto del pincel
-        int anchoPincel = 1; //ancho del pincel
+        int altoPincel = 2; //alto del pincel
+        int anchoPincel = 2; //ancho del pincel
 
         //Gestionar el color
         SolidColorBrush colorPintar = new SolidColorBrush(Color.FromArgb(255,0,0,0));
@@ -50,8 +55,12 @@ namespace Paintiris
         List<ToggleButton> pincelColor = new List<ToggleButton>();
         List<ToggleButton> herramienta = new List<ToggleButton>();
 
-        //**************************************** CONSTRUCTOR ****************************************************
+        //********************************************************************************************
+                    /*  ____ ____ _  _ ____ ___ ____ _  _ ____ ___ ____ ____ 
+                        |    |  | |\ | [__   |  |__/ |  | |     |  |  | |__/ 
+                        |___ |__| | \| ___]  |  |  \ |__| |___  |  |__| |  \ */
 
+        //********************************************************************************************
         public MainWindow()
         {
             InitializeComponent();
@@ -63,17 +72,19 @@ namespace Paintiris
             pinceles = new Pinceles(this);
 
             //llenamos colecciones para que al activar uno se desactiven todos
-            llenarColecciones();
+            LlenarColecciones();
 
-            //ponemos color al inicio del pincel
-            colorPin = Color.FromArgb(255, 0, 0, 0);
-            cv_colorPintar.Background = colorPintar;
 
         }
 
-        //**************************************** EVENTOS DE COMPONENTES *******************************************
+        //********************************************************************************************
+        /*  ____ _  _ ____ _  _ ___ ____ ____    ___  ____    ____ ____ _  _ ___  ____ _  _ ____ _  _ ___ ____ 
+            |___ |  | |___ |\ |  |  |  | [__     |  \ |___    |    |  | |\/| |__] |  | |\ | |___ |\ |  |  |___ 
+            |___  \/  |___ | \|  |  |__| ___]    |__/ |___    |___ |__| |  | |    |__| | \| |___ | \|  |  |___  */
 
-        //BOTONES__________________________________________________________________
+        //********************************************************************************************
+
+        //BOTONES GESTIÓN__________________________________________________________________
 
         /// <summary>
         /// Click de los elementos de archivo => NUEVO, GUARDAR, GUARDARCOMO, ABRIR
@@ -118,29 +129,51 @@ namespace Paintiris
             }
         }
 
+        //BOTONES COLOR__________________________________________________________________
 
         /// <summary>
-        /// Gestionamos el cambio de color, al pulsar nos abre un formulario nuevo para elegir el color
+        /// Con el botón derecho del ratón cambiamos el color que se encuentra en ese recuadro
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        private void CambioColor_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            Canvas color = (Canvas)sender;
+
             //le mandamos al dialog el color que tenemos en el canvas al inicio
             ColorDialog miColor = new ColorDialog(colorPintar.Color);
             miColor.ShowDialog();
+
             if (miColor.DialogResult == true)
             {
                 colorPintar = new SolidColorBrush(miColor.color);
-                cv_colorPintar.Background = colorPintar;
-                //si el pincel está seleccionado, volvemos a crearlo o no se actualizará el nuevo color
-                if (pintarActivado == true)
-                {
-                    lienzo.EditingMode = InkCanvasEditingMode.Ink;
-                    lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
-                }
+                color.Background = colorPintar;
+                Pintar();
             }
         }
+
+        /// <summary>
+        /// Al pulsar con el botón izquierdo del ratón elegimos el color con el que 
+        /// queremos pintar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ElegirColor_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Canvas canvas = (Canvas)sender;
+
+            //si el pincel está seleccionado, volvemos a crearlo o no se actualizará el nuevo color
+            if (pintarActivado)
+            {
+                //hacemos esta conversión para poder meter el color del canvas en el pincel, primero lo pasamos a brush y luego a colorSolidBrush
+                Brush colorDelCanvas = canvas.Background;
+                colorPintar = (SolidColorBrush)colorDelCanvas;
+                
+                Trace.WriteLine("entro");
+                Pintar();
+            }
+        }
+
 
 
         //prueba a botones como radio button
@@ -160,6 +193,10 @@ namespace Paintiris
             {
                 case "tbtn_borrar":
                     Trace.WriteLine("borrar");
+                    foreach (ToggleButton boton in pincelTamano)
+                    {
+                        boton.IsEnabled = false;
+                    }
                     lienzo.EditingMode = InkCanvasEditingMode.EraseByPoint;
                     //propiedades de la goma
                     lienzo.EraserShape = new EllipseStylusShape(1, 1);
@@ -175,6 +212,13 @@ namespace Paintiris
             }
         }
 
+        //TAMAÑO PINCEL__________________________________________________________________
+        
+        /// <summary>
+        /// Determinamos el tamño del pincel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void pintar_checked(object sender, RoutedEventArgs e)
         {
             ToggleButton tbtn = (ToggleButton)sender;
@@ -187,65 +231,64 @@ namespace Paintiris
                     boton.IsChecked = false;
                 }
             }
-            switch (tbtn.Name)
-            {
-                case "tbtn_pincel1px":
-                    altoPincel = 1;
-                    break;
-                case "tbtn_pincel3px":
-                    altoPincel = 3;
-                    break;
-                case "tbtn_pincel6px":
-                    altoPincel = 6;
-                    break;
-                case "tbtn_pincel10px":
-                    altoPincel = 10;
-                    break;
-                case "tbtn_pincel13px":
-                    altoPincel = 13;
-                    break;
-               
-
-                default:
-                    break;
-            }
+            altoPincel = Convert.ToInt16(tbtn.Tag);
 
             pintarActivado = true;
-            lienzo.EditingMode = InkCanvasEditingMode.Ink;
-            lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
+
+            //si está marcado que es un pincel, dividimos el alto entre dos para hacerlo de forma ovalada
+            Trace.WriteLine(siPincel);
+            
+
+            Pintar();
         }
 
+        /// <summary>
+        /// Elegimos si queremos lápiz, igual ancho que alto o el pincel
+        /// cuyo ancho será aproximadamente la mitad del alto
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void pincelOlapiz_Checked(object sender, RoutedEventArgs e)
         {
             ToggleButton tbtn = (ToggleButton)sender;
-            // si le da a uno los otros los desmarcamos
-            foreach (ToggleButton boton in herramienta)
+
+            //activamos los tamaños
+            foreach (ToggleButton boton in pincelTamano)
             {
-                if (boton.Name != tbtn.Name)
-                {
-                    boton.IsChecked = false;
-                }
+                
+                boton.IsEnabled = true;
             }
 
-            //si está marcado que es un pincel, dividimos el alto entre dos para hacerlo de forma ovalada
+            // si le da a uno los otros los desmarcamos
+            ActivasDesactivar(tbtn, herramienta);
+
+            //activamos que estamos dibujando
+            pintarActivado = true;
+
             if (tbtn.Name == "tbtn_pincel")
             {
+
                 siPincel = true;
-                anchoPincel = altoPincel / 2;
             }
             else
             {
-                siPincel = false;
-                anchoPincel = altoPincel;
+               siPincel = false;
             }
-            lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
+           
+            Pintar();
 
         }
 
-        //********************************************* FUNCIONES ************************************************
+        //********************************************************************************************
+                        /*  ____ _  _ _  _ ____ _ ____ _  _ ____ ____ 
+                            |___ |  | |\ | |    | |  | |\ | |___ [__  
+                            |    |__| | \| |___ | |__| | \| |___ ___]  */
 
+        //********************************************************************************************
+
+        //trasladarlas luego
         //LLENAR Colecciones__________________________________________________________________
-        private void llenarColecciones()
+        private void LlenarColecciones()
         {
             prueba.Add(tbtn_borrar);
             prueba.Add(tbtn_selec);
@@ -259,6 +302,43 @@ namespace Paintiris
             herramienta.Add(tbtn_lapiz);
             herramienta.Add(tbtn_pincel);
         }
+
+        /// <summary>
+        /// Funciona como un Refresh() cada vez que hacemos un cambio en color o forma llamamos a la función para 
+        /// evitar repetir código
+        /// </summary>
+        private void Pintar()
+        {
+            if (siPincel)
+            {
+                anchoPincel = altoPincel / 2;
+                if (anchoPincel < 1)
+                {
+                    anchoPincel = 1;
+                }
+            }
+            else
+            {
+
+                anchoPincel = altoPincel;
+            }
+
+            lienzo.EditingMode = InkCanvasEditingMode.Ink;
+            
+            lienzo.DefaultDrawingAttributes = pinceles.PintarPincel(altoPincel, anchoPincel, colorPintar.Color);
+        }
+
+        private void ActivasDesactivar(ToggleButton boton, List<ToggleButton> lista)
+        {
+            foreach (ToggleButton botonElemento in herramienta)
+            {
+                if (botonElemento.Name != boton.Name)
+                {
+                    botonElemento.IsChecked = false;
+                }
+            }
+        }
+
     }
 
 
